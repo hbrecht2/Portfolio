002dd9e2e47ab4cd9e415ce2466aa23db4c5a999
Hannah Brecht <hbrecht2@live.maryville.edu>
2020-09-17 00:18:21 -0400

    Resolved bug with SMTP PHPMailer

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output -- Disabled now to prevent messages from appearing to clients 
-    $mail->isSMTP();                                            // Send using SMTP
+    //$mail->isSMTP();                                            // Send using SMTP  --Disabled for heroku deployment
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = getenv('personalEmail');                     // SMTP username
