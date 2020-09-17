31b687c8de4c972a0c817804d64ce82f64e9526c
Hannah Brecht <hbrecht2@live.maryville.edu>
2020-09-17 00:24:37 -0400

    Resolved bug with SMTP PHPMailer

--- mail.php
+++ mail.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = getenv('personalEmail');                     // SMTP username
-    $mail->Password   = getenv('personalEmailPassword');                               // SMTP password
+    $mail->Username   = getenv("personalEmail");                     // SMTP username
+    $mail->Password   = getenv("personalEmailPassword");                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
